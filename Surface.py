a844c4a6fd4c9bba8f2538b5b7d12aaccce4f207
Daly Olivert <92411950+dalyo1212@users.noreply.github.com>
2022-04-16 20:51:37 +0000

    Finished Lab

---
+++ Surface.py
@@ -0,0 +1,9 @@
+from Rectangle import Rectangle
+class Surface:
+  def __init__(self, filename, x, y, height, width):
+    self.rect = Rectangle(x, y, height, width)
+    self.image = filename
+
+  def getRect(self):
+    return self.rect
+    
